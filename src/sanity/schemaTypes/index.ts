609a251224fdc30a1abbd9b6f07b52f830a918f9
WebUnica Chile <javier.millar@gmail.com>
2026-02-02 18:28:17 -0300

    Feat: Add Blog Post schema example

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -3,7 +3,8 @@ import { heroType } from './hero'
 import { featureType } from './feature'
 import { testimonialType } from './testimonial'
 import { logoGridType } from './logoGrid'
+import { postType } from './post'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-    types: [heroType, featureType, testimonialType, logoGridType],
+    types: [heroType, featureType, testimonialType, logoGridType, postType],
 }
---
+++ src/sanity/schemaTypes/post.ts
@@ -0,0 +1,47 @@
+import { defineField, defineType } from 'sanity'
+
+export const postType = defineType({
+    name: 'post',
+    title: 'Blog Post',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        }),
+        defineField({
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                maxLength: 96,
+            },
+        }),
+        defineField({
+            name: 'author',
+            title: 'Author',
+            type: 'string',
+        }),
+        defineField({
+            name: 'mainImage',
+            title: 'Main image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        }),
+        defineField({
+            name: 'publishedAt',
+            title: 'Published at',
+            type: 'datetime',
+        }),
+        defineField({
+            name: 'body',
+            title: 'Body',
+            type: 'array',
+            of: [{ type: 'block' }],
+        }),
+    ],
+})
