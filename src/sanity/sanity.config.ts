23d3c4c52f648dae40c5f1ab154d6ddb583db1d1
WebUnica Chile <javier.millar@gmail.com>
2026-02-02 18:54:29 -0300

    Refactor: Move sanity.config.ts to src/sanity/ for cleaner path resolution

--- src/sanity/sanity.config.ts
+++ src/sanity/sanity.config.ts
@@ -8,9 +8,8 @@ import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from './src/sanity/env'
-import { schema } from './src/sanity/schemaTypes'
+import { apiVersion, dataset, projectId } from './env'
+import { schema } from './schemaTypes'
 
 export default defineConfig({
     basePath: '/studio',
